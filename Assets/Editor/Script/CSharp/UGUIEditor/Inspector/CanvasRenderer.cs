b55921c706bfec954728bfe108d9005c64831c39
NobuGaga <biweixiong@yinhandeiMac.local>
2019-10-29 11:45:59 +0800

    2019-10-29_Commit Canvas Renderer Inspector Class

---
+++ Assets/Editor/Script/CSharp/UGUIEditor/Inspector/CanvasRenderer.cs
@@ -0,0 +1,11 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace UGUIEditor {
+
+    [CustomEditor(typeof(CanvasRenderer))]
+    internal class CanvasRendererInspector : Editor {
+
+        public override void OnInspectorGUI() { }
+    }
+}

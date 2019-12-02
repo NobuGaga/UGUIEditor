d03cbdaac0a12028a7d034db2b483d81e9d6a938
NobuGaga <biweixiong@yinhandeiMac.local>
2019-12-02 22:12:40 +0800

    2019-12-02_Update Shell Run Logic

--- Assets/Editor/Script/CSharp/Shell/Shell.cs
+++ Assets/Editor/Script/CSharp/Shell/Shell.cs
@@ -1,15 +1,36 @@
+using UnityEngine;
 using System;
 using System.Text;
 using System.Diagnostics;
+using UGUIEditor;
 
 public static class Shell {
 
+    private static string ShellPath = Tool.GetCacheString(Application.dataPath + 
+#if UNITY_EDITOR_WIN
+    "\\Editor\\Script\\.Bat\\"
+#elif UNITY_EDITOR_OSX
+   "/Editor/Script/.Shell/"
+#else
+    string.Empty
+#endif
+    );
+
     private static StringBuilder m_builder = new StringBuilder();
 
-    public static void StartBat(string batPath, params object[] args) {
+    public static void Run(string shellName, params object[] args) {
+        string shellPath = ShellPath + shellName +
+#if UNITY_EDITOR_WIN
+    ".bat"
+#elif UNITY_EDITOR_OSX
+   ".sh"
+#else
+    string.Empty
+#endif
+        ;
         ProcessStartInfo info = new ProcessStartInfo();
-        info.FileName = batPath;
-        if (args != null) {
+        info.FileName = shellPath;
+        if (args != null && args.Length > 0) {
             m_builder.Clear();
             for (ushort index = 0; index < args.Length; index++) {
                 m_builder.Append(args[index]);
@@ -24,7 +45,7 @@ public static class Shell {
             process.Start();
             process.WaitForExit();
         } catch (Exception exception) {
-            UnityEngine.Debug.LogError("Shell::StartBat exception " + exception);
+            UnityEngine.Debug.LogError("Shell::Run exception " + exception + "\nshell name " + shellPath);
         } finally {
             process.Close();
         }

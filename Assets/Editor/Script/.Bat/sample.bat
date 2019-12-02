11c7f429299c434ae53ae965a55aa32f0011bfa1
NobuGaga <biweixiong@yinhandeiMac.local>
2019-12-02 20:53:47 +0800

    2019-12-02_Add UTF-8 Shell

---
+++ Assets/Editor/Script/.Bat/sample.bat
@@ -0,0 +1,4 @@
+set UNITY_PATH=C:\software\Unity2019.1.4\Editor\Unity.exe
+set PROJECT_PATH=D:\svn\common\client\now\UIProject
+set METHOD_NAME=UGUIEditor.EditorMenu.UnpackAllPrefabInstance
+%UNITY_PATH% -projectPath %PROJECT_PATH% -quit -batchmode -executeMethod %METHOD_NAME%
---
+++ Assets/Editor/Script/.Shell/sample.sh
@@ -0,0 +1,4 @@
+export UNITY_PATH=/Applications/Unity/Hub/Editor/2019.2.9f1/unity.app/Contents/MacOS/Unity
+export PROJECT_PATH=/Users/biweixiong/Documents/GitHub/Unity/UGUIEditor
+export METHOD_NAME=UGUIEditor.EditorMenu.UnpackAllPrefabInstance
+$UNITY_PATH -projectPath $PROJECT_PATH -quit -batchmode -executeMethod $METHOD_NAME
